c9c77b928a8cc3e4eee0c34eb57b4b5d6a40d3d8
DarunRG <darunrg11@gmail.com>
2025-09-01 19:27:18 +0530

    Update download_dataset.py

--- download_dataset.py
+++ download_dataset.py
@@ -1,5 +1,7 @@
+#install kagglehub in your system using "pip install kagglehub" in command prompt
 import kagglehub
 
 path = kagglehub.dataset_download("emmarex/plantdisease")
+
 
 print("Path to dataset files:", path)
