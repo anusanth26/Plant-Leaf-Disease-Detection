df8c585d0fde16fd1df2dcff8d1e3ada78545e61
anusanth26 <anusanth2645@gmail.com>
2025-09-15 23:03:10 +0530

    moved ML models into Machine-Learning folder

--- download_dataset.py
+++
@@ -1,7 +0,0 @@
-#install kagglehub in your system using "pip install kagglehub" in command prompt
-import kagglehub
-
-path = kagglehub.dataset_download("emmarex/plantdisease")
-
-
-print("Path to dataset files:", path)
